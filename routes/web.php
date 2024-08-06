7550cd1906a234d9055e147012e2910ba58c0d60
shreejanpandit <shreezanpandit@gmail.com>
2024-08-06 16:13:36 +0545

    [ADD]: comments added

--- routes/web.php
+++ routes/web.php
@@ -5,7 +5,7 @@ use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class , 'index'])->name("home");
+Route::get('/', [HomeController::class , 'index'])->name("home"); //named route
 Route::get('/about', [AboutController::class , 'index'])->name("about");
 Route::get('/contact', [ContactController::class , 'index'])->name("contact");
-Route::post('/contact/{id}', [ContactController::class , 'store']);
+Route::post('/contact/{id}', [ContactController::class , 'store']);  //route with id parameter
