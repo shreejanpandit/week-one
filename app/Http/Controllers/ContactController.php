8193f5b121c4760d28afff20e9e1cc827e3a937b
shreejanpandit <shreezanpandit@gmail.com>
2024-08-06 16:12:23 +0545

    [ADD]: added store for post method

--- app/Http/Controllers/ContactController.php
+++ app/Http/Controllers/ContactController.php
@@ -6,8 +6,12 @@ use Illuminate\Http\Request;
 
 class ContactController extends Controller
 {
-    public function index(int $id)
+    public function index()
     {
         return view('protfolio.contact');
+    }
+
+    public function store(int $id){
+        //
     }
 }
