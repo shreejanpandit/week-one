bc8c38fc70db118f6a58b60b996e09d44269f368
shreejanpandit <shreezanpandit@gmail.com>
2024-08-06 16:08:29 +0545

    [DEL]: deleted contact and about method

--- app/Http/Controllers/HomeController.php
+++ app/Http/Controllers/HomeController.php
@@ -12,15 +12,5 @@ class HomeController extends Controller
         $personal_details = ["name" => "Shreejan Pandit", "address" => "Pokhara-32", "personal_image" => $personal_image,"learn"=>"laravel"];
         return view('protfolio.home', ["my_details" => $personal_details]);
     }
-
-    public function about()
-    {
-        $git_link = ["git"=>"https://github.com/shreejanpandit/","gmail"=>"shreezanpandit@gmail.com"];
-        return view('protfolio.about', ["link" => $git_link]);
-    }
-
-    public function contact()
-    {
-        return view('protfolio.contact');
-    }
+    
 }
