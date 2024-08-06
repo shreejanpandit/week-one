1b74450e599139813f68e587667114cc7ec0e302
shreejanpandit <shreezanpandit@gmail.com>
2024-08-06 16:09:11 +0545

    [IMP]: seprate controller and route for different methods

--- routes/web.php
+++ routes/web.php
@@ -1,8 +1,11 @@
 <?php
 
+use App\Http\Controllers\AboutController;
+use App\Http\Controllers\ContactController;
 use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\HomeController;
 
-Route::get('/', [HomeController::class , 'index']);
-Route::get('/about', [HomeController::class , 'about']);
-Route::get('/contact', [HomeController::class , 'contact']);
+Route::get('/', [HomeController::class , 'index'])->name("home");
+Route::get('/about', [AboutController::class , 'index'])->name("about");
+Route::get('/contact', [ContactController::class , 'index'])->name("contact");
+Route::post('/contact/{id}', [ContactController::class , 'store']);
